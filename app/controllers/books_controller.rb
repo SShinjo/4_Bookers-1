d9440ec5d43ff9171b5202db8595aa9abc6228cc
SShinjo <skskosmn@gmail.com>
2019-11-07 14:21:58 +0900

    [add]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,26 @@ class BooksController < ApplicationController
   end
 
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(book)
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
