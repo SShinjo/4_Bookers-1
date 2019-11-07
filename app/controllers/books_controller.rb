cf5cdf43ff940015cc62defec2dfa0dfd0c9d586
SShinjo <skskosmn@gmail.com>
2019-11-07 15:07:19 +0900

    [add]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,18 +7,36 @@ class BooksController < ApplicationController
   	@book = Book.new
   end
 
+  def create
+  	book = Book.new(book_params)
+  	if book.save
+  		flash[:notice] = "Book was successfully created."
+  	end
+  	redirect_to book_path(book)
+
+
+  end
+
   def show
   	@book = Book.find(params[:id])
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
 
-  def create
-  	book = Book.new(book_params)
-  	book.save
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
   	redirect_to book_path(book)
   end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
 
   private
 
